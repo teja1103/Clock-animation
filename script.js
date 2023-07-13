3654c47c93d7a872986d2a3d271abe09dba3d1a0
teja1103 <suryatejaprogramming@gmail.com>
2023-07-13 21:59:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate(){
+    const now= new Date();
+    const seconds= now.getSeconds(); //gets the real time second
+    const secondsDegrees = ((seconds / 60) * 360) +90; //calculating the number degrees the second hand should move
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`; //applying the rotation style to the seconds hand
+    console.log(seconds);
+    const minutes = now.getMinutes();//gets the real time minutes
+    const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) +90;//calculating the number degrees the minute hand should move
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;//applying the rotation style to the minutes hand
+    console.log(minutes);
+    const hours = now.getHours();//gets the real time hour
+    const hoursDegrees = ((hours / 12) * 360)+ ((minutes/60)*30) +90;//calculating the number degrees the second hand should move
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;//applying the rotation style to the hours hand
+    console.log(hours);
+} 
+
+
+setInterval(setDate,1000);
