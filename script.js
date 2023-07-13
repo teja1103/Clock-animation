9623b924824112fa3da3b9df59f722936a5e2bdb
teja1103 <suryatejaprogramming@gmail.com>
2023-07-13 22:07:41 +0530

    second commit

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ function setDate(){
     const seconds= now.getSeconds(); //gets the real time second
     const secondsDegrees = ((seconds / 60) * 360) +90; //calculating the number degrees the second hand should move
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`; //applying the rotation style to the seconds hand
-    console.log(seconds);
+    console.log(seconds + "seconds");
     const minutes = now.getMinutes();//gets the real time minutes
     const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) +90;//calculating the number degrees the minute hand should move
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;//applying the rotation style to the minutes hand
-    console.log(minutes);
+    console.log(minutes + "minutes");
     const hours = now.getHours();//gets the real time hour
     const hoursDegrees = ((hours / 12) * 360)+ ((minutes/60)*30) +90;//calculating the number degrees the second hand should move
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;//applying the rotation style to the hours hand
-    console.log(hours);
+    console.log(hours + "hours");
 } 
 
 
